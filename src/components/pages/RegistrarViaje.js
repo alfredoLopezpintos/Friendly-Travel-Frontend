d5d8b7767b1b172fc0030bc565ea5ef150014786
Alfredo <alfredodaniel87617@gmail.com>
2022-10-17 14:35:59 -0300

    Agregadas validaciones del formulario

--- src/components/pages/RegistrarViaje.js
+++ src/components/pages/RegistrarViaje.js
@@ -6,27 +6,67 @@ import axios from 'axios';
 import { Button2 } from '../Button2';
 import { Link } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
+import moment from 'moment';
 
 export default function RegistrarViaje() {
 
   const { register, handleSubmit } = useForm();
-  //const onSubmit = (data, e) => console.log(data);
   const onSubmit = (data, e) => fetchViajes(data, e);
   const onError = (errors, e) => console.log(errors, e);
   const redirect = (data, e) => redirect2(data, e);
   const history = useHistory();
   
+  function isNumber(str) {
+    if (str.trim() === '') {
+      return false;
+    }
+  
+    return !isNaN(str);
+  }
+
+  function formValidate(data) {
+    const dateObj = new Date();
+    const month = dateObj.getUTCMonth() + 1; //months from 1-12
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const today = year + "-" + month + "-" + day;
+
+    if(data.tripDate === "" ||
+     data.source === "" ||
+     data.price === "" ||
+     data.destination === "" ||
+     data.availablePlaces === "") {
+        alert("Debe llenar todos los campos para poder crear el viaje.")
+        return false;
+    }else if (!isNumber(data.price)){
+      alert("El precio debe ser un número.")
+    }else if (!isNumber(data.availablePlaces)){
+      alert("Lugares disponibles debe ser un número.")
+    }else if (!moment(data.tripDate).isValid()){
+      //console.log(moment(data.tripDate))
+      alert("Fecha inválida.")
+    }else if (moment(data.tripDate) < moment(today)){
+      alert("La fecha del viaje no puede ser anterior al día actual.")
+    }else {
+      return true;
+    }
+  }
+
   async function fetchViajes(data, e) {
-    const viajesGetEndpoint = configData.AWS_REST_ENDPOINT + "/trips"
+
+    if(formValidate(data)) {
+      const viajesGetEndpoint = configData.AWS_REST_ENDPOINT + "/trips"
     
-    try {
-      //const response = await axios.get(viajesGetEndpoint);
-      const response = await axios.post(viajesGetEndpoint, data);
-      console.log(response)
-      redirect();
-      //setViajes(response.data);
-    } catch(error) {
-      console.error(error);
+      try {
+        //const response = await axios.get(viajesGetEndpoint);
+        const response = await axios.post(viajesGetEndpoint, data);
+        console.log(response)
+        redirect();
+        //setViajes(response.data);
+      } catch(error) {
+        console.error(error);
+        //alert('Error inesperado');
+      }
     }
   }
 
@@ -40,12 +80,12 @@ export default function RegistrarViaje() {
       <form onSubmit={handleSubmit(onSubmit, onError)}>
           
           <div className = "field1">
-          <label> Agregar Viaje </label>
+          <label> Nuevo Viaje </label>
           <input {...register("source")} placeholder="Origen"/>
           <input {...register("destination")} placeholder="Destino"/>
           <input {...register("tripDate")} placeholder="Fecha"/>
           <input {...register("availablePlaces")} placeholder="Lugares Disponibles"/>
-          <input {...register("price")} placeholder="Precio"/>
+          <input {...register("price")} placeholder="Precio (En Pesos Uruguayos)"/>
           </div>
 
           <br />
