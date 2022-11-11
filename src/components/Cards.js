0869855206ad8d1e8db771046b5686acae7d0c84
Diego Rosales <diegorosales@Diegos-MacBook-Pro.local>
2022-11-11 12:19:20 -0300

    arreglo url imagenes y titulo

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,12 +10,12 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/colonia.jpg'
+              src={require("../assets/images/colonia.jpg")}
               text='Viaja y explora su Barrio Histórico con calles de adoquines'
               label='Colonia'
             />
             <CardItem
-              src='images/faro.jpg'
+              src={require("../assets/images/faro.jpg")}
               text='La costa este de Uruguay es una sucesión de balnearios con paisajes tan variados como maravillosos'
               label='Costa Atlántica'
 
@@ -23,19 +23,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/punta.jpg'
+              src={require("../assets/images/punta.jpg")}
               text='Es uno de los centros turísticos más importante y elegidos del país'
               label='Punta del Este'
 
             />
             <CardItem
-              src='images/traffic.jpg'
+              src={require("../assets/images/traffic.jpg")}
               text='Explora la capital del país y sus sitios más iconicos'
               label='Montevideo'
  
             />
             <CardItem
-              src='images/auto.jpg'
+              src={require("../assets/images/auto.jpg")}
               text='¡Viaja, explora, conoce nuestro país y haz nuevos amigos!'
               label='Aventura'
 
